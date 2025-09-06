9c68db05d9c6d0515f1797c6cb184aa5110f2a90
Sreekanthp001 <sreekanthpaleti1999@gmail.com>
2025-09-06 16:52:57 +0530

    python

--- vars/pythonEKSPipeline.groovy
+++ vars/pythonEKSPipeline.groovy
@@ -31,6 +31,7 @@ def call(Map configMap){
                 steps {
                     script {
                         sh """
+                            sudo yum install -y python3-devel gcc
                             pip3 install -r requirements.txt
                         """
                     }
