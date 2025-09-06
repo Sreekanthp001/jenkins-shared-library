bfe53b7b16bc7b031286da4732198f308ac8e97a
Sreekanthp001 <sreekanthpaleti1999@gmail.com>
2025-09-06 16:36:18 +0530

    jenkins

--- vars/javaEKSPipeline.groovy
+++ vars/javaEKSPipeline.groovy
@@ -1,6 +1,6 @@
 def call(Map configMap){
     pipeline {
-        agent {
+        agent  {
             label 'AGENT-1'
         }
         environment {
@@ -11,7 +11,7 @@ def call(Map configMap){
             COMPONENT = configMap.get('component')
         }
         options {
-            timeout(time: 30, unit: 'MINUTES')
+            timeout(time: 30, unit: 'MINUTES') 
             disableConcurrentBuilds()
         }
         parameters {
@@ -21,71 +21,75 @@ def call(Map configMap){
         stages {
             stage('Read pom.xml') {
                 steps {
-                    appVersion = readMavenPom().getVersion()
-                    echo "app version: ${appVersion}"
-                }
-            }
-        }
-        stage('Install Dependencies') {
-            steps {
-                script {
-                    sh """
-                        mvn clean package
-                    """
-                }
-            }
-        }
-        stage('Unit Testing') {
-            steps {
-                script {
-                    sh """
-                        echo "unit tests"
-                    """
-                }
-            }
-        }
-        stage('Docker Build') {
-            steps {
-                script {
-                    withAWS(credentials: 'aws-creds', region: 'us-east-1') {
-                        sh """
-                            aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
-                            docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} .
-                            docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
-                            #aws ecr wait image-scan-complete --repository-name ${PROJECT}/${COMPONENT} --image-id imageTag=${appVersion} --region ${REGION}
-                        """
+                    script {
+                        appVersion = readMavenPom().getVersion()
+                        echo "app version: ${appVersion}"
                     }
                 }
             }
-        }
-        stage('Trigger Deploy') {
-            when{
-                expression { params.deploy }
-            }
-            steps {
-                script {
-                    //build job: 'catalogue-cd',
-                    build job: "../${COMPONENT}-cd",
-                    parameters: [
-                        string(name: 'appVersion', value: "${appVersion}"),
-                        string(name: 'deploy_to', value: 'dev')
-                    ],
-                    propagate: false, //even sg fails vpc will not be effected
-                    wait: false // vpc will not wait for sg pipeline completion
+            stage('Install Dependencies') {
+                steps {
+                    script {
+                    sh """
+                        mvn clean package 
+                    """
+                    }
                 }
             }
+            stage('Unit Testing') {
+                steps {
+                    script {
+                    sh """
+                            echo "unit tests"
+                    """
+                    }
+                }
+            }
+            stage('Docker Build') {
+                steps {
+                    script {
+                        withAWS(credentials: 'aws-creds', region: 'us-east-1') {
+                            sh """
+                                aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
+                                docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} .
+                                docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
+                                #aws ecr wait image-scan-complete --repository-name ${PROJECT}/${COMPONENT} --image-id imageTag=${appVersion} --region ${REGION}
+                            """
+                        }
+                    }
+                }
+            }
+            stage('Trigger Deploy') {
+                when{
+                    expression { params.deploy }
+                }
+                steps {
+                    script {
+                        //build job: 'catalogue-cd',
+                        build job: "../${COMPONENT}-cd",
+                        parameters: [
+                            string(name: 'appVersion', value: "${appVersion}"),
+                            string(name: 'deploy_to', value: 'dev')
+                        ],
+                        propagate: false,  // even SG fails VPC will not be effected
+                        wait: false // VPC will not wait for SG pipeline completion
+                    }
+                }
+            }
+            
         }
-    }
-    post {
-        always {
-            echo 'I will always say Hello again!'
-            deleteDir()
-        }
-        success {
-            echo 'Hello Success'
-        }
-        failure {
-            echo 'Hello Failure'
+
+        post { 
+            always { 
+                echo 'I will always say Hello again!'
+                deleteDir()
+            }
+            success { 
+                echo 'Hello Success'
+            }
+            failure { 
+                echo 'Hello Failure'
+            }
         }
     }
 }
