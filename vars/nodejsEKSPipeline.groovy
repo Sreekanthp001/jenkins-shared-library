7bc07dbf6c07d49157e94400cbcddcff29df1e3a
Sreekanthp001 <sreekanthpaleti1999@gmail.com>
2025-09-07 20:36:14 +0530

    fronend-ci

--- vars/nodejsEKSPipeline.groovy
+++ vars/nodejsEKSPipeline.groovy
@@ -53,9 +53,8 @@ def call(Map configMap){
                         withAWS(credentials: 'aws-creds', region: 'us-east-1') {
                             sh """ 
                                 aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
-                                sudo docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} .
-                                sudo docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
-
+                                docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} .
+                                docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
 
                             """
                         }
